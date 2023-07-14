88b3d745496d335258b4497583897702945bb1f0
Shaik Shameem <128825551+8799ucs@users.noreply.github.com>
2023-07-15 00:00:41 +0530

    Update settings.py

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'example'
+    'example.Templates'
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'vercel_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['example'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
