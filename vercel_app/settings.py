9c099983d5771ff68d823dd99086be5ce3472c42
Shaik Shameem <128825551+8799ucs@users.noreply.github.com>
2023-07-17 16:02:08 +0530

    Update settings.py

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'example.Templates'
+    'example.Templates',
+    'accounts',
 ]
 
 MIDDLEWARE = [
