e2af7489a818e1b58c10aac9b745bf8ff8272248
Shaik Shameem <128825551+8799ucs@users.noreply.github.com>
2023-07-17 19:00:33 +0530

    Update views.py

--- example/views.py
+++ example/views.py
@@ -7,13 +7,13 @@ def login_user(request):
     if(request.method=='POST'):
         username=request.POST['username']
         password=request.POST['password']
-        user=authentication(request, username=username, password=password)
+        user=authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             return redirect('Templates/student-login.html')
         else:
-            return render(request,'index.html',{'error':'Invalid username or password'})
-return render(request,'index.html')
+            return render(request,'Templates/index.html',{'error':'Invalid username or password'})
+return render(request,'Templates/index.html')
 
 def my_view(request):
     return render(request, 'Templates/index.html')
