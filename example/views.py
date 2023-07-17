b63d4c172086c047339960e50bdd8c9bc5e9f31f
Shaik Shameem <128825551+8799ucs@users.noreply.github.com>
2023-07-17 19:20:45 +0530

    Update views.py

--- example/views.py
+++ example/views.py
@@ -1,5 +1,5 @@
 # example/views.py
-from django.contrib.auth.models import User
+from django.contrib.auth.models import Student
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render,redirect
 
