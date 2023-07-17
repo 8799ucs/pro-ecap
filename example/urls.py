a423c27a85009cf15a345a9b7398429ed6f4c967
Shaik Shameem <128825551+8799ucs@users.noreply.github.com>
2023-07-17 23:45:48 +0530

    Update urls.py

--- example/urls.py
+++ example/urls.py
@@ -1,8 +1,9 @@
 # example/urls.py
 from django.urls import path
-from example.views import my_view, my_data, login_user
+from example.views import my_view, create_user, login_user
 
 urlpatterns = [
     path('', my_view),
+    path('',create_user),
     path('login/',login_user),
 ]
