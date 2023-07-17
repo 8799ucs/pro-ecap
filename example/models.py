5fec20f783732ec71851053b5bc0abf7f46ff0d7
Shaik Shameem <128825551+8799ucs@users.noreply.github.com>
2023-07-17 19:27:48 +0530

    Update models.py

--- example/models.py
+++ example/models.py
@@ -1,5 +1,8 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
-class Student(models.Model):
+
+
+class Student(AbstractUser):
   username=models.CharField(max_length=100)
   password=models.CharField(max_length=100)
   name=models.CharField(max_length=100)
