d8ea7c94d548a0954ba2f22341e60d7d57f084ee
Shaik Shameem <128825551+8799ucs@users.noreply.github.com>
2023-07-17 11:42:27 +0530

    Update urls.py

--- example/urls.py
+++ example/urls.py
@@ -1,10 +1,10 @@
 # example/urls.py
-from django.urls import path,include
+from django.urls import path, include
 from django.contrib import admin
-from example.views import my_view,my_data
+from example.views import my_view, my_data
 
 urlpatterns = [
     path('admin/',admin.site.urls),
-    path('index/', my_view),
+    path('', my_view),
     path('student-login/',my_data),
 ]
