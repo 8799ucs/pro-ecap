29ed842ad49d4fe810de90aecea2d3c738372589
Shaik Shameem <128825551+8799ucs@users.noreply.github.com>
2023-07-17 20:34:41 +0530

    Update views.py

--- example/views.py
+++ example/views.py
@@ -1,10 +1,12 @@
 # example/views.py
-from django.contrib.auth.models import Student
+from .models import Student
 from django.contrib.auth import authenticate, login
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 
 def login_user(request):
     if request.method=='POST' :
+        username = request.POST['username']
+        password = request.POST['password']
         user=authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
