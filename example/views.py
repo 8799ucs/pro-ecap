8fb0a8c031b37dde1ca64258a3ab66af95b52241
Shaik Shameem <128825551+8799ucs@users.noreply.github.com>
2023-07-17 19:09:49 +0530

    Update views.py

--- example/views.py
+++ example/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth import authenticate, login
 from django.shortcuts import render,redirect
 
 def login_user(request):
-    if(request.method=='POST'):
+    if request.method=='POST' :
         username=request.POST['username']
         password=request.POST['password']
         user=authenticate(request, username=username, password=password)
