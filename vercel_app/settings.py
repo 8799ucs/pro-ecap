a0181aa106ac412d576fe1bea0f30f0e4c54d1b9
Shaik Shameem <128825551+8799ucs@users.noreply.github.com>
2023-07-17 22:04:24 +0530

    Update settings.py

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'vercel_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['example'],
+        'DIRS': [os.path.join(BASE_DIR, 'Templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
