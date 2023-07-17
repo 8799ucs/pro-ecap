1cf55d79d7b906dfdf0c9f558c93c361ac87b38b
Shaik Shameem <128825551+8799ucs@users.noreply.github.com>
2023-07-17 18:38:06 +0530

    Update urls.py

--- example/urls.py
+++ example/urls.py
@@ -7,6 +7,6 @@ from example.views import my_view, my_data
 urlpatterns = [
     path('admin/',admin.site.urls),
     path('', my_view),
-    path('login/',login_user)
+    path('login/',login_user),
     path('Templates/student-login/',my_data),
 ]
