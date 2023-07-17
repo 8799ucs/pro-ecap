41dcf463196aed37230a7c20504ad10ede679043
Shaik Shameem <128825551+8799ucs@users.noreply.github.com>
2023-07-17 10:04:05 +0530

    Update views.py

--- example/views.py
+++ example/views.py
@@ -1,5 +1,10 @@
 # example/views.py
 from django.shortcuts import render
+from .models import Student
 
 def my_view(request):
-    return render(request, 'Templates/index.html')
+    return render(request, 'templates/index.html')
+
+def my_data(request):
+    data=Student.objects.all()
+    return render(request,'templates/student-login.html')
