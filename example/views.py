339693dafe7d11576b293011fd73c45335d9e3ad
Shaik Shameem <128825551+8799ucs@users.noreply.github.com>
2023-07-17 19:11:32 +0530

    Update views.py

--- example/views.py
+++ example/views.py
@@ -5,15 +5,13 @@ from django.shortcuts import render,redirect
 
 def login_user(request):
     if request.method=='POST' :
-        username=request.POST['username']
-        password=request.POST['password']
         user=authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             return redirect('Templates/student-login.html')
         else:
             return render(request,'Templates/index.html',{'error':'Invalid username or password'})
-return render(request,'Templates/index.html')
+    return render(request,'Templates/index.html')
 
 def my_view(request):
     return render(request, 'Templates/index.html')
