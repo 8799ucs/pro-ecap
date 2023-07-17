c2e34592e4264374013226e4fef1c911207f346c
Shaik Shameem <128825551+8799ucs@users.noreply.github.com>
2023-07-17 15:55:17 +0530

    Update views.py

--- example/views.py
+++ example/views.py
@@ -1,6 +1,19 @@
 # example/views.py
 from django.contrib.auth.models import Studentuser
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render,redirect
+
+def login_user(request):
+    if(request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        user=authentication(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('Templates/student-login.html')
+        else:
+            return render(request,'index.html',{'error':'Invalid username or password'})
+return render(request,'index.html')
 
 def my_view(request):
     return render(request, 'Templates/index.html')
