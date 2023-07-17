76b47261593a2d8381ad3969dec7e677a892e69f
Shaik Shameem <128825551+8799ucs@users.noreply.github.com>
2023-07-17 15:49:19 +0530

    Update views.py

--- example/views.py
+++ example/views.py
@@ -1,8 +1,19 @@
 # example/views.py
-from django.shortcuts import render
+from django.contrib.auth.models import Studentuser
+from django.shortcuts import render,redirect
 
 def my_view(request):
     return render(request, 'Templates/index.html')
+
+def create_stu(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        name=request.POST['name']
+        phone=request.POST['phone']
+        stuser=Studentuser.objects.create_user(username=username, password=password, name=name, phone=phone)
+        stuser.save()
+        return redirect('student-login.html')
 
 def my_data(request):
     data=Student.objects.all()
