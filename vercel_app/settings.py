96e12e7ce408c715317a58e40ddeb8c93df62ac9
Shaik Shameem <128825551+8799ucs@users.noreply.github.com>
2023-07-18 15:15:24 +0530

    Update settings.py

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=cldztbc4jg&xl0!x673!*v2_=p$$eu)=7*f#d0#zs$44xx-h^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'vercel_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'Templates')],
+        'DIRS': ['example/Templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
