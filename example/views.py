0c9477cc9e35a311c81897075e1b6dcbb4cbc621
Shaik Shameem <128825551+8799ucs@users.noreply.github.com>
2023-07-18 12:36:19 +0530

    Update views.py

--- example/views.py
+++ example/views.py
@@ -1,35 +1,14 @@
 # example/views.py
-from examples.models import Student
-from django.contrib.auth import authenticate, login
-from django.shortcuts import render, redirect
+from django.shortcuts import render
+from example.models import Student, Teacher
 
+def student_list(request):
+    students = Student.objects.all()
+    return render(request, 'student-login.html', {'students': students})
 
-def create_user(request):
-    # Get a reference to the Firestore database
-    db = firestore.client()
-
-    # Perform database operations
-    doc_ref = db.collection('users').document('new_user')
-    doc_ref.set({
-        'username': '21241A6737',
-        'password': '21241A6737',
-        'name':'Nagula Navya sri',
-        'phone':'7893762846',
-    })
-
-
-def login_user(request):
-    if request.method=='POST' :
-        username = request.POST['username']
-        password = request.POST['password']
-        user=authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('Templates/student-login.html')
-        else:
-            return render(request,'Templates/index.html',{'error':'Invalid username or password'})
-    else :
-        return render(request,'Templates/index.html')
+def teacher_list(request):
+    teachers = Teacher.objects.all()
+    return render(request, 'teacher-login.html', {'teachers': teachers})
 
 def my_view(request):
     return render(request, 'Templates/index.html')
