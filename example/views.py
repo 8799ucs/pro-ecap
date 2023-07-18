bf6c2b6a6f23fbc45c49ab4335f75136a5ceb9b8
Shaik Shameem <128825551+8799ucs@users.noreply.github.com>
2023-07-18 14:41:36 +0530

    Update views.py

--- example/views.py
+++ example/views.py
@@ -4,11 +4,11 @@ from example.models import Student, Teacher
 
 def student_list(request):
     students = Student.objects.all()
-    return render(request, 'student_list.html', {'students': students})
+    return render(request, 'Template/student_list.html', {'students': students})
 
 def teacher_list(request):
     teachers = Teacher.objects.all()
-    return render(request, 'teacher_list.html', {'teachers': teachers})
+    return render(request, 'Templates/teacher_list.html', {'teachers': teachers})
 
 def my_view(request):
     return render(request, 'Templates/index.html')
