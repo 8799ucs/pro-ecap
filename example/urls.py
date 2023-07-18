d8a309a314a407a711aa9320b2e8d4264d2f0073
Shaik Shameem <128825551+8799ucs@users.noreply.github.com>
2023-07-18 22:22:32 +0530

    Update urls.py

--- example/urls.py
+++ example/urls.py
@@ -4,7 +4,5 @@ from example.views import my_view,student_list,teacher_list ,create_user
 app_name='example'
 urlpatterns = [
     path('', my_view),
-    path('', student_list),
-    path('', teacher_list),
     path('',create_user),
 ]
