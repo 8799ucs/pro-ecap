f3889ab9e2de637219eeb088cc1f54acfd11fbb1
Shaik Shameem <128825551+8799ucs@users.noreply.github.com>
2023-07-18 22:03:49 +0530

    Update models.py

--- example/models.py
+++ example/models.py
@@ -3,12 +3,14 @@ from django.db import models
 
 
 class Student(AbstractUser):
+  is_student = models.BooleanField(default=True)
   username=models.CharField(max_length=100)
   password=models.CharField(max_length=100)
   name=models.CharField(max_length=100)
   phone=models.CharField(max_length=100)
 
 class Teacher(AbstractUser):
+  is_student = models.BooleanField(default=False)
   username=models.CharField(max_length=100)
   password=models.CharField(max_length=100)
   name=models.CharField(max_length=100)
