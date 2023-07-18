4660ce95cdb58d672263b5359565dff21ebdf7bf
Shaik Shameem <128825551+8799ucs@users.noreply.github.com>
2023-07-18 22:07:28 +0530

    Update urls.py

--- example/urls.py
+++ example/urls.py
@@ -1,9 +1,10 @@
 # example/urls.py
 from django.urls import path,include
-from example.views import my_view,student_list,teacher_list  
+from example.views import my_view,student_list,teacher_list ,create_user 
 app_name='example'
 urlpatterns = [
     path('', my_view),
     path('', student_list),
     path('', teacher_list),
+    path('',create_user),
 ]
