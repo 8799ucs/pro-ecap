e54bfbafc82a74b9d3521ab47a592275fccd913e
Shaik Shameem <128825551+8799ucs@users.noreply.github.com>
2023-07-18 12:41:20 +0530

    Update admin.py

--- example/admin.py
+++ example/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from example.models import Student
+from example.models import Student, Teacher
 
-# Register your models here.
 admin.site.register(Student)
+admin.site.register(Teacher)
+
