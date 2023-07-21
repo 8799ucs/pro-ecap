64513164cf72c93fa3a0f29d0129a64013704872
SimhadriMajji <137772752+SimhadriMajji@users.noreply.github.com>
2023-07-21 17:14:31 +0530

    Update views.py

--- example/views.py
+++ example/views.py
@@ -4,9 +4,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from example.models import Student, Teacher
 from django.http import JsonResponse
-from django.contrib.auth.forms import AuthenticationForm
-from example import create,tcreate 
-from . import *
+from django.contrib.auth.forms import AuthenticationForm 
+from example import *
 
 def view(request):
     return render(request,'templates/index.html')
