1c17b0ce8f830a88241a5c8fc15711e008c09dc4
SimhadriMajji <137772752+SimhadriMajji@users.noreply.github.com>
2023-07-21 17:35:08 +0530

    Update views.py

--- example/views.py
+++ example/views.py
@@ -35,7 +35,7 @@ def sign_view(request):
     if request.method == 'POST':
         username = request.POST.get('username1','')
         password = request.POST.get('password1','')
-        dbase = sqlite3.connect('tdata.db')
+        dbase = sqlite3.connect('bdata.db')
         cursor = dbase.cursor()
         cursor.execute("SELECT * FROM Teacher WHERE username = ?", (username,))
         user_data = cursor.fetchone()
